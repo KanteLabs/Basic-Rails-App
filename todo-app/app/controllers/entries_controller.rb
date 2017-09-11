e70e63aab3a6dfb0b31cc01186bc0e9a8721a8f4
Amidou Kante <kamdiou@gmail.com>
2017-09-11 16:39:56 -0400

    entries controller

--- todo-app/app/controllers/entries_controller.rb
+++ todo-app/app/controllers/entries_controller.rb
@@ -1,2 +1,32 @@
 class EntriesController < ApplicationController
+    def index
+        @entries = Entry.all
+    end
+
+    def show
+        @entry = Entry.find_by(id: params[:id])
+    end
+
+    def create
+        entry = params['entry']
+        Entry.create(
+            title: Entry[:title],
+            description: Entry[:description]
+        )
+        redirect_to entries_path
+    end
+
+    def update
+        Entry.create(
+            params[:id],
+            title: Entry[:title],
+            description: Entry[:description]
+        )
+        redirect_to entries_path            
+    end
+
+    def destroy
+        Entry.destroy(params['id'])
+        redirect_to entries_path
+    end
 end
